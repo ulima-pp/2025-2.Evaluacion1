c4d28cb28169ace2a2e3e773ac0cd033ead30901
ProfN405 <ProfN405@lab.aca>
2025-10-03 11:59:37 -0500

    Comenzando implementacion de Colisiones (OO)

--- src/colisiones.h
+++ src/colisiones.h
@@ -2,10 +2,51 @@
 
 #include <glm/vec2.hpp>
 
-struct Rectangulo
-{
-    glm::vec2 anchoAlto;
-    glm::vec2 centerPosition;
-};
+namespace Fisicas {
+    struct Forma {};
 
-bool ColisionRectangular(const Rectangulo& r1, const Rectangulo& r2);
+    struct Rectangulo : public Forma
+    {
+        glm::vec2 anchoAlto;
+        glm::vec2 centerPosition;
+    };
+
+    struct Circulo  : public Forma
+    {
+        glm::vec2 centerPosition;
+        float radio;
+    };
+
+    class GestorColisiones
+    {
+        Colision* m_Colision;
+        public:
+            GestorColisiones();
+            void SetColision(Colision colision);
+            bool Resolver(Forma forma1, Forma forma2);
+    };
+
+    class Colision
+    {
+        public:
+            virtual bool Resolver(Forma forma1, Forma forma2) = 0;
+    };
+
+    class ColisionRectangular : public Colision
+    {
+        public:
+            bool Resolver(Forma forma1, Forma forma2) override;
+    };
+
+    class ColisionCircular : public Colision
+    {
+        public:
+            bool Resolver(Forma forma1, Forma forma2) override;
+    };
+    
+    bool ColisionRectangular(const Rectangulo& r1, const Rectangulo& r2);
+}
+
+
+
+
